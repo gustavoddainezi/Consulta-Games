d146adafda4f3da2a2cbca2655378c051a315df1
Gustavo D. Dainezi <gustavo.dainezi@fatec.sp.gov.br>
2020-09-14 14:22:11 -0300

    Projeto criado

---
+++ backend/src/main/java/com/biggamesurvey/consultagames/ConsultagamesApplication.java
@@ -0,0 +1,13 @@
+package com.biggamesurvey.consultagames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultagamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultagamesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/biggamesurvey/consultagames/ConsultagamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biggamesurvey.consultagames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultagamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
